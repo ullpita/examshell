40f332b6d13bea627edcfac6d9f27866879e2549
ullpita <16001888+ullpita@users.noreply.github.com>
2018-02-02 16:57:29 +0100

    Update fprime.c

--- fprime.c
+++ fprime.c
@@ -5,46 +5,47 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: upierre- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/15 12:13:13 by upierre-          #+#    #+#             */
-/*   Updated: 2018/01/15 13:26:21 by upierre-         ###   ########.fr       */
+/*   Created: 2018/02/02 16:16:54 by upierre-          #+#    #+#             */
+/*   Updated: 2018/02/02 16:55:56 by upierre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int		main(int ac, char **av)
 {
-	int		i;
 	int		nbr;
+	int		i;
 
-	if(ac != 2)
+	if (ac != 2)
 	{
 		printf("\n");
 		return(0);
 	}
 	nbr = atoi(av[1]);
-	if(nbr == 1)
+	if (nbr == 1)
 	{
 		printf("1\n");
 		return(0);
 	}
-	while(1)
+	while (1)
 	{
 		i = 1;
 		while (i++ <= nbr)
 		{
-			if (nbr % i == 0)
+			if ((nbr % i) == 0)
 			{
 				printf("%d", i);
 				nbr = nbr / i;
 				break;
 			}
 		}
-		if (nbr == 1)
+		if (nbr == 1 || nbr == 0)
 			break;
 		else
 			printf("*");
 	}
 	printf("\n");
+	return(0);
 }
